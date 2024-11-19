d9c08b5e38dec8ad86e197afe2011a81ac88fd40
RishiChaubey31 <cenationrishi31@gmail.com>
2024-11-19 05:36:27 +0530

    UI structuring

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -30,11 +30,14 @@ export const getProducts = async (rq, res) => {
 
 export const deleteProducts = async (req, res) => {
   const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ success: false, message: "Invalid product id" });
+  }
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "product deleted" });
   } catch (error) {
-    res.status(404).json({ success: false, message: "product not found" });
+    res.status(500).json({ success: false, message: "server error" });
   }
 };
 export const updateProducts = async (req, res) => {
